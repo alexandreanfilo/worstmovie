51a74b05c12b7db1e40aa866e71563220c2459f8
Alexandre H. Anfilo <alexandreanfilo@gmail.com>
2021-12-26 17:35:23 -0300

    Cleaning movie controller and repo

--- src/main/java/com/texoit/worstmovie/MovieRepository.java
+++ src/main/java/com/texoit/worstmovie/MovieRepository.java
@@ -3,23 +3,11 @@ package com.texoit.worstmovie;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
    public List<Movie> findByWinner(boolean winner);
-
-   @Query(nativeQuery = true, value = "select p.name as producer"
-         + "    from movies m"
-         + "   inner join movies_producers mp"
-         + "      on mp.movie_id = m.id"
-         + "   inner join producers p"
-         + "      on p.id = mp.producers_id"
-         + "   where m.winner = true"
-         + "   group by p.name"
-         + "  having count(*) > 1")
-   public List<ConsecutiveAwardsStatistic> findConsecutiveAwards();
 
 }
