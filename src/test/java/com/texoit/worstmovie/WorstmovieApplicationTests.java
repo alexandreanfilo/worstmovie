164265c883e746fd0484f538422027b8c00d59d9
Alexandre H. Anfilo <alexandreanfilo@gmail.com>
2022-07-20 14:56:38 -0300

    Tests - improved file content validation

--- src/test/java/com/texoit/worstmovie/WorstmovieApplicationTests.java
+++ src/test/java/com/texoit/worstmovie/WorstmovieApplicationTests.java
@@ -8,65 +8,110 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.apache.commons.csv.CSVRecord;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class WorstmovieApplicationTests {
 
+	private final String fileName = "movielist.csv";
+
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
-		System.out.println("contextLoads...");
-	}
+	@Autowired
+	private CSVService csvService;
 
 	@Test
 	public void shouldReturnAllMovies() throws Exception {
-		// todo: Ler arquivo e validar se todos os MOVIES retornados pela API batem com do arquivo
+		Set<String> movies = new HashSet<String>();
 
-		this.mockMvc.perform(get("/movies/all"))
+		ResultActions result = this.mockMvc.perform(get("/movies/all"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$", hasSize(206)))
-				.andExpect(jsonPath("$[0].title", is("Can't Stop the Music")));
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+		for (CSVRecord row : this.csvService.getCSVRecords(this.fileName)) {
+			String title = this.csvService.findStringValue(row, "title");
+
+			if (!movies.contains(title)) {
+				movies.add(title);
+			}
+		}
+
+		result.andExpect(jsonPath("$", hasSize(movies.size())));
+
+		for (int i = 0; i < movies.size(); i++) {
+			result.andExpect(jsonPath("$[" + i + "].title", in(movies)));
+		}
 	}
 
 	@Test
 	public void shouldReturnAllProducers() throws Exception {
-		// todo: Ler arquivo e validar se todos os PRODUCERS retornados pela API batem com do arquivo
+		Set<String> producers = new HashSet<String>();
 
-		this.mockMvc.perform(get("/producers/all"))
+		ResultActions result = this.mockMvc.perform(get("/producers/all"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$", hasSize(359)))
-				.andExpect(jsonPath("$[0].name", is("Allan Carr")));
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+		for (CSVRecord row : this.csvService.getCSVRecords(this.fileName)) {
+			List<String> producersNames = this.csvService.findSplittedValues(row, "producers", ",| and ");
+
+			for (String name : producersNames) {
+				if (!producers.contains(name)) {
+					producers.add(name);
+				}
+			}
+		}
+
+		result.andExpect(jsonPath("$", hasSize(producers.size())));
+
+		for (int i = 0; i < producers.size(); i++) {
+			result.andExpect(jsonPath("$[" + i + "].name", in(producers)));
+		}
 	}
 
 	@Test
 	public void shouldReturnAllStudios() throws Exception {
-		// todo: Ler arquivo e validar se todos os STUDIOS retornados pela API batem com do arquivo
+		Set<String> studios = new HashSet<String>();
 
-		this.mockMvc.perform(get("/studios/all"))
+		ResultActions result = this.mockMvc.perform(get("/studios/all"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$", hasSize(59)))
-				.andExpect(jsonPath("$[0].name", is("Associated Film Distribution")));
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+		for (CSVRecord row : this.csvService.getCSVRecords(this.fileName)) {
+			List<String> studiosNames = this.csvService.findSplittedValues(row, "studios", ",| and ");
+
+			for (String name : studiosNames) {
+				if (!studios.contains(name)) {
+					studios.add(name);
+				}
+			}
+		}
+
+		result.andExpect(jsonPath("$", hasSize(studios.size())));
+
+		for (int i = 0; i < studios.size(); i++) {
+			result.andExpect(jsonPath("$[" + i + "].name", in(studios)));
+		}
 	}
 
 	@Test
 	public void shouldReturnConsecutiveAwards() throws Exception {
-		// todo: Refazer calculo e validar se bate com os valores retornados pela API		
-
 		this.mockMvc.perform(get("/producers/consecutive-awards"))
 				.andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
